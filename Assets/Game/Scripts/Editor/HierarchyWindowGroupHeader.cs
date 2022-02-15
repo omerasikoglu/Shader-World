53ea1c38e3ccd55ff9f08c22996ba8de2e6bac75
OmerAsik <faruk.asikoglu@gmail.com>
2022-02-15 18:33:25 +0300

    Refactor: Better HierarchyWindowGroupHeader

--- Assets/Game/Scripts/Editor/HierarchyWindowGroupHeader.cs
+++ Assets/Game/Scripts/Editor/HierarchyWindowGroupHeader.cs
@@ -1,9 +1,12 @@
 using UnityEditor;
 using UnityEngine;
 
-// <summary>
-/// Hierarchy Window Group Header
-/// http://diegogiacomelli.com.br/unitytips-hierarchy-window-group-header
+/// <summary>
+/// Colorful Hierarchy Window Group Header
+/// Author: https://github.com/omerasikoglu
+/// Thanks for concept of idea : http://diegogiacomelli.com.br/unitytips-hierarchy-window-group-header
+/// And special thanks for social media post: github.com/farukcan
+/// Samples: "#9856CC HEADER" , "#magenta header2" , "# HEADER3", #9856cc header5 "#yElLoW hEaDeR4,"
 /// </summary>
 [InitializeOnLoad]
 public static class HierarchyWindowGroupHeader
@@ -17,10 +20,46 @@ public static class HierarchyWindowGroupHeader
     {
         var gameObject = EditorUtility.InstanceIDToObject(instanceID) as GameObject;
 
-        if (gameObject != null && gameObject.name.StartsWith("---", System.StringComparison.Ordinal))
+        if (gameObject != null && gameObject.name.StartsWith("#", System.StringComparison.Ordinal))
         {
-            EditorGUI.DrawRect(selectionRect, Color.black);
-            EditorGUI.DropShadowLabel(selectionRect, gameObject.name.Replace("-", "").ToUpperInvariant());
+            // format is #color and #FF0012
+            string colorName = gameObject.name.Substring(1).Split(' ')[0];
+
+            // convert colorname to unity color
+            Color color = IsHexCode() ? GetColorFromString(colorName) :
+                (ColorUtility.TryParseHtmlString(colorName.ToLower(), out var _color) ? _color : Color.black);
+
+            EditorGUI.DrawRect(selectionRect, color);
+            EditorGUI.DropShadowLabel(selectionRect, gameObject.name.Replace($"#{colorName}", string.Empty).ToUpperInvariant());
+
+            bool IsHexCode()
+            {
+                if (colorName.Length == 0) return false;
+                //check digit or hex letters
+                for (int i = 0; i < colorName.Length; i++)
+                {
+                    if (!char.IsDigit(colorName, i))
+                    {
+                        if (char.ToUpper(colorName[i]).Equals('A') || char.ToUpper(colorName[i]).Equals('B')
+                        || char.ToUpper(colorName[i]).Equals('C') || char.ToUpper(colorName[i]).Equals('D')
+                        || char.ToUpper(colorName[i]).Equals('E') || char.ToUpper(colorName[i]).Equals('F'))
+                            continue;
+
+                        return false;
+                    }
+                }
+                return true;
+            }
+
+            Color GetColorFromString(string color)
+            {
+                float red = System.Convert.ToInt32(color.Substring(0, 2), 16) / 255f;
+                float green = System.Convert.ToInt32(color.Substring(2, 2), 16) / 255f;
+                float blue = System.Convert.ToInt32(color.Substring(4, 2), 16) / 255f;
+
+                return new Color(red, green, blue);
+            }
         }
     }
+
 }
